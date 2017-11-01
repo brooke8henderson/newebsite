98b1cecaf618533628d43cc66e96779fee62f613
brooke8henderson <31804715+brooke8henderson@users.noreply.github.com>
2017-10-31 23:25:59 -0400

    all major changes and validations done

--- js/myScript.js
+++ js/myScript.js
@@ -1,20 +1,25 @@
+$(document).ready(function () { // keep this at top
 
-function enlarge(id) { //selects the image with the id that we want to enlarge
-  $(id).click(function () {      //sets when function will run
-    $(id).animate({width: '100%'})   //details of function action
-  })
+var ids = ['#one', '#two', '#three', '#four']; // array of ids
+
+function enlarge(selector) { //selects the image with the id that we want to enlarge
+  $(selector).animate({width: '100%'})   //details of function action
+};
+
+function shrink(selector) {
+  $(selector).animate({width: '50%'})
+};
+
+// loop over array of ids on mouseover enlarge and mouseleave shrink
+// don't need to call ids individually.
+//"This" is neeeded to set where the function will happen
+for (var id of ids) {
+  $( id )
+    .mouseover(function() {
+      enlarge(this);
+    })
+    .mouseout(function() {
+      shrink(this)
+    });
 }
-
-function shrink(id) {
-  $(id).click(function () {
-    $(id).animate({width: '25%'})
-  })
-}
-
-
-$(document).ready(function() {  //this part sets what image is effected and how
-  enlarge('#one')
-  shrink('#two')
-  enlarge('#three')
-  shrink('#four')
-});
+}); // end of document ready.
