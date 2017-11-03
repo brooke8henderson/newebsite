d8da41fe3e4afa27db8ae11118d14fa5ab81ca85
brooke8henderson <31804715+brooke8henderson@users.noreply.github.com>
2017-11-03 12:45:09 -0400

    final commit

--- js/myScript.js
+++ js/myScript.js
@@ -2,17 +2,17 @@ $(document).ready(function () { // keep this at top
 
 var ids = ['#one', '#two', '#three', '#four']; // array of ids
 
-function enlarge(selector) { //selects the image with the id that we want to enlarge
-  $(selector).animate({width: '100%'})   //details of function action
+function enlarge(selector) { //selects the image with the id that i want to enlarge
+  $(selector).animate({width: '100%'})   //details of function action, animate how
 };
 
-function shrink(selector) {
-  $(selector).animate({width: '50%'})
+function shrink(selector) {                // selects what to shrink
+  $(selector).animate({width: '50%'})   /// how much to shrink
 };
 
 // loop over array of ids on mouseover enlarge and mouseleave shrink
 // don't need to call ids individually.
-//"This" is neeeded to set where the function will happen
+//"This" is neeeded to set where the function will happen, makes it so don't need each id, like the for loop
 for (var id of ids) {
   $( id )
     .mouseover(function() {
